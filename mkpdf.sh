84a0eec6bcb946fbe6ee559e3da496f9e936e3a0
Kazufumi Ohkawa <kerolinq@gmail.com>
2019-02-23 13:20:37 +0900

    Add pdf

---
+++ mkpdf.sh
@@ -0,0 +1,14 @@
+cat \
+ch01_introduction.asciidoc \
+ch02_installation.asciidoc \
+ch03_python.asciidoc \
+ch04_database.asciidoc \
+ch05_rdkit.asciidoc \
+ch06_similarity.asciidoc \
+ch07_graph.asciidoc \
+ch08_visualization.asciidoc \
+ch09_qsar.asciidoc \
+ch10_deeplearning.asciidoc \
+ch11_dlqsar.asciidoc \
+ch12_generativemodels.asciidoc > py4c.asciidoc
+asciidoctor-pdf -r asciidoctor-pdf-cjk -a pdf-style=py4c-theme.yml -o pdf/py4chemoinformatics.pdf py4c.asciidoc
